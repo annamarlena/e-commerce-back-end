6c6ce77b99916c459bbe69f4003804e23eb9e6f7
Anna Marlena <annamarlena310@gmail.com>
2023-05-08 21:58:20 -0500

    Started adding the missing code

--- models/Category.js
+++ models/Category.js
@@ -1,16 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+      //   * Doesn't allow null values.
+      //   * Set as primary key.
+      //   * Uses auto increment.
+    id: {
+      type: DataTypes.INTEGER  
+    },
+    categoryName: {
+      type: DataTypes.STRING   //   * Doesn't allow null values.
+    }
   },
   {
     sequelize,
-    timestamps: false,
+    timestamps: true,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
